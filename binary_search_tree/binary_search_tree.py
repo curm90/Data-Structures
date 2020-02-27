0689fbcfbd060bb7c51fa859987f2457b4c6e731
Liam Sutton <devliam90@gmail.com>
2020-02-27 09:46:20 +0000

    refactor to use consistent if statements

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -11,15 +11,15 @@ class BinarySearchTree:
     # Insert the given value into the tree
     def insert(self, value):
         if value < self.value:
-            if self.left is not None:
-                self.left.insert(value)
-            else:
+            if not self.left:
                 self.left = BinarySearchTree(value)
-        elif value >= self.value:
-            if self.right is not None:
-                self.right.insert(value)
             else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
                 self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -27,17 +27,17 @@ class BinarySearchTree:
         if target == self.value:
             return True
         elif target < self.value:
-            if self.left is None:
+            if not self.left:
                 return False
             return self.left.contains(target)
         else:
-            if self.right is None:
+            if not self.right:
                 return False
             return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if self.right is None:
+        if not self.right:
             return self.value
         return self.right.get_max()
 
